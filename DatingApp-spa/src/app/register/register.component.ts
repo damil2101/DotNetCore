20172b429cf01eb337819a9655b66a276d68c108
Damil <damilpanchal@DamilsMacBook.local>
2020-05-13 15:05:10 -0400

    Third party libraries

--- DatingApp-spa/src/app/register/register.component.ts
+++ DatingApp-spa/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,7 @@ export class RegisterComponent implements OnInit {
   model:any = {};
   @Output() cancelRegister = new EventEmitter();
 
-  constructor(private auth:AuthService) { }
+  constructor(private auth:AuthService, private alertify:AlertifyService) { }
 
   ngOnInit() {
   }
@@ -18,16 +19,16 @@ export class RegisterComponent implements OnInit {
   register(){
     this.auth.register(this.model)
       .subscribe(next => {
-        console.log("registered successfully");
+        this.alertify.success("registered successfully");
       },
       error => {
-        console.log("error in registration"+error)
+        this.alertify.error(error);
       })
 
   }
 
   cancel(){
     this.cancelRegister.emit(false);
-    console.log("cancel");
+    this.alertify.message("dismissed");
   }
 }
